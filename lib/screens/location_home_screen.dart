9c16453b57504cfcdccca1fd62790917980ce94b
fanolans <mailenolan@gmail.com>
2023-01-31 22:10:39 +0000

    add permission service and get location

--- lib/screens/location_home_screen.dart
+++ lib/screens/location_home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationHomeScreen extends StatefulWidget {
   const LocationHomeScreen({super.key});
@@ -8,6 +9,8 @@ class LocationHomeScreen extends StatefulWidget {
 }
 
 class _LocationHomeScreenState extends State<LocationHomeScreen> {
+  LocationData? _locationData;
+  final Location _location = Location();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +19,32 @@ class _LocationHomeScreenState extends State<LocationHomeScreen> {
       ),
       body: Column(
         children: [
+          if (_locationData != null)
+            Text('${_locationData!.latitude}, ${_locationData!.longitude}'),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () async {
+              bool serviceEnabled = await _location.serviceEnabled();
+              if (!serviceEnabled) {
+                serviceEnabled = await _location.requestService();
+                if (!serviceEnabled) {
+                  return;
+                }
+              }
+              PermissionStatus permissionStatus =
+                  await _location.hasPermission();
+              if (permissionStatus == PermissionStatus.denied) {
+                permissionStatus = await _location.requestPermission();
+                if (permissionStatus == PermissionStatus.denied) {
+                  return;
+                }
+              }
+              final locationData = await _location.getLocation();
+              print(locationData.latitude);
+              print(locationData.longitude);
+              setState(() {
+                _locationData = locationData;
+              });
+            },
             child: const Text('Get location'),
           ),
         ],
