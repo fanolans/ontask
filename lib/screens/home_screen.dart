e0289bc37489aaad6a2e2b501716a3a9f0b8a019
fanolans <mailenolan@gmail.com>
2023-02-27 01:10:52 +0700

    extract widget NewTodo

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:ontask/services/database_service.dart';
+import 'package:ontask/widgets/new_todo.dart';
 import 'package:ontask/widgets/todo_list.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,9 +11,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  String _todoTitle = '';
-  final _todoTitleController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,48 +27,11 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Container(
         child: Column(
-          children: [
-            const Expanded(
+          children: const [
+            Expanded(
               child: TodoList(),
             ),
-            Container(
-              padding: const EdgeInsets.all(12),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _todoTitleController,
-                      decoration: InputDecoration(
-                        hintText: 'Saya akan...',
-                        border: OutlineInputBorder(
-                          borderSide: const BorderSide(width: 0.5),
-                          borderRadius: BorderRadius.circular(25),
-                        ),
-                        contentPadding: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 0),
-                      ),
-                      onChanged: (value) {
-                        setState(() {
-                          _todoTitle = value;
-                        });
-                      },
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: _todoTitle.isEmpty
-                        ? null
-                        : () {
-                            DatabaseService().addNewTitle(_todoTitle);
-                            _todoTitleController.clear();
-                          },
-                    style: ElevatedButton.styleFrom(
-                      shape: const CircleBorder(),
-                    ),
-                    child: const Icon(Icons.add),
-                  ),
-                ],
-              ),
-            ),
+            NewTodo(),
           ],
         ),
       ),
