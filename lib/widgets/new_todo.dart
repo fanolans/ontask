9a2911ccbff650c9c9d9f66b24c26d3f61f5a540
fanolans <mailenolan@gmail.com>
2023-02-27 01:11:06 +0700

    create NewTodo widget

---
+++ lib/widgets/new_todo.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../services/database_service.dart';
+
+class NewTodo extends StatefulWidget {
+  const NewTodo({super.key});
+
+  @override
+  State<NewTodo> createState() => _NewTodoState();
+}
+
+class _NewTodoState extends State<NewTodo> {
+  String _todoTitle = '';
+  final _todoTitleController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _todoTitleController,
+              decoration: InputDecoration(
+                hintText: 'Saya akan...',
+                border: OutlineInputBorder(
+                  borderSide: const BorderSide(width: 0.5),
+                  borderRadius: BorderRadius.circular(25),
+                ),
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _todoTitle = value;
+                });
+              },
+            ),
+          ),
+          ElevatedButton(
+            onPressed: _todoTitle.isEmpty
+                ? null
+                : () {
+                    DatabaseService().addNewTitle(_todoTitle);
+                    _todoTitleController.clear();
+                  },
+            style: ElevatedButton.styleFrom(
+              shape: const CircleBorder(),
+            ),
+            child: const Icon(Icons.add),
+          ),
+        ],
+      ),
+    );
+  }
+}
