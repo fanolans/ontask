36cc9c40572a9b8bab927d87253c9d0c9b4a85c6
fanolans <mailenolan@gmail.com>
2023-02-28 19:57:34 +0700

    create static future getUserLocation function

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -1,6 +1,27 @@
+import 'package:location/location.dart';
+
 const googleApiKey = 'AIzaSyBym3WxDcjYJRf5iqUcdeHh8MZbcBj4we4';
 
 class LocationService {
+  static Future<LocationData?> getUserLocation() async {
+    Location location = Location();
+    bool serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return null;
+      }
+    }
+    PermissionStatus permissionStatus = await location.hasPermission();
+    if (permissionStatus == PermissionStatus.denied) {
+      permissionStatus = await location.requestPermission();
+      if (permissionStatus == PermissionStatus.denied) {
+        return null;
+      }
+    }
+    return location.getLocation();
+  }
+
   static String generateMapUrlImage({
     double? latitude,
     double? longitude,
