e2bcc02bf59258c59e7b87cfb35764f2e72d5a8c
fanolans <mailenolan@gmail.com>
2023-02-28 19:35:55 +0700

    create map screen

---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
+import '../models/place_location.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key, required this.initialLocaion});
+  final PlaceLocation initialLocaion;
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  LatLng _pickedLocation = LatLng(-6.1754, 106.8272);
+  String _address = '';
+
+  Future getUserLocation() async {
+    Location location = Location();
+    location.getLocation();
+  }
+
+  @override
+  void initState() {
+    if (widget.initialLocaion.latitude == 0 &&
+        widget.initialLocaion.longitude == 0) {
+      getUserLocation();
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
