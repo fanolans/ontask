6e780b724e5c3f81b963c6d1a2f2039c70e4cdac
fanolans <mailenolan@gmail.com>
2023-03-21 17:24:20 +0700

    create function getPlaceAddress

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
 
 const googleApiKey = 'AIzaSyBym3WxDcjYJRf5iqUcdeHh8MZbcBj4we4';
 
@@ -28,5 +31,19 @@ class LocationService {
   }) {
     if (latitude == null || longitude == null) return '';
     return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=15&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$googleApiKey';
+  }
+
+  static Future<String> getPlaceAddress(
+      double latitude, double longitude) async {
+    final url =
+        'https://maps.googleapis.com/maps/api/geocode/json?latlng=$latitude,$longitude,key=$googleApiKey';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final results = json.decode(response.body);
+      if (results['results'].lenghh > 0) {
+        return results['results'][0]['formatted_address'];
+      }
+    }
+    return '';
   }
 }
