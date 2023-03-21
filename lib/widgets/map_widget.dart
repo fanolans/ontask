0ff61462c95ca9b4c0824a8c45b9a079702d55f8
fanolans <mailenolan@gmail.com>
2023-03-21 17:25:17 +0700

    add GestureDetector and setLocationFn

--- lib/widgets/map_widget.dart
+++ lib/widgets/map_widget.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:ontask/models/place_location.dart';
+import 'package:ontask/screens/map_screen.dart';
 
 import '../services/location_service.dart';
 
 class MapWidget extends StatelessWidget {
-  const MapWidget({super.key, required this.placeLocation});
+  const MapWidget({
+    super.key,
+    required this.placeLocation,
+    required this.setLocationFn,
+  });
   final PlaceLocation placeLocation;
+  final Function(PlaceLocation placeLocation) setLocationFn;
 
   @override
   Widget build(BuildContext context) {
@@ -13,18 +19,30 @@ class MapWidget extends StatelessWidget {
       latitude: placeLocation.latitude,
       longitude: placeLocation.longitude,
     );
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        border: Border.all(width: 0.5),
-        borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (builder) => MapScreen(
+              initialLocaion: placeLocation,
+              setLocationFn: setLocationFn,
+            ),
+          ),
+        );
+      },
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          border: Border.all(width: 0.5),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: previewMapUrlImage.isEmpty
+            ? const Center(
+                child: Text('Tap untuk menambahkan lokasi'),
+              )
+            : Image.network(previewMapUrlImage),
       ),
-      child: previewMapUrlImage.isEmpty
-          ? const Center(
-              child: Text('Tap untuk menambahkan lokasi'),
-            )
-          : Image.network(previewMapUrlImage),
     );
   }
 }
