d3b2d9d0effdd34dbb4383ef2fc5a04ab55c1955
fanolans <mailenolan@gmail.com>
2023-03-22 23:21:25 +0700

    remove unused container

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -12,99 +12,108 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              height: 150,
-              color: Colors.blueAccent,
-              child: Row(
-                children: [
-                  const CircleAvatar(
-                    minRadius: 40,
-                    child: Icon(
-                      Icons.person,
-                      size: 30,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 15,
-                  ),
-                  const Text(
-                    'Farrel Nolan',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.edit),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 10,
-              ),
-              child: Text(
-                'Data User',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Card(
-                child: Container(
-                  padding: const EdgeInsets.all(20),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Container(
-                            child: const Text('Email'),
-                          ),
-                          const Text(': '),
-                          const Text('mailenolan@gmail.com')
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                            child: const Text('Email'),
-                          ),
-                          const Text(': '),
-                          const Text('mailenolan@gmail.com')
-                        ],
-                      )
-                    ],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            height: 150,
+            color: Colors.blueAccent,
+            child: Row(
+              children: [
+                const CircleAvatar(
+                  minRadius: 40,
+                  child: Icon(
+                    Icons.person,
+                    size: 30,
+                    color: Colors.white,
                   ),
                 ),
-              ),
+                const SizedBox(
+                  width: 15,
+                ),
+                const Text(
+                  'Farrel Nolan',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.edit),
+                ),
+              ],
             ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: ElevatedButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Text('Logout'),
-                    Icon(Icons.logout),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            child: Text(
+              'Data User',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Card(
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Row(
+                      children: const [
+                        SizedBox(
+                          width: 100,
+                          child: Text('Email'),
+                        ),
+                        Text(': '),
+                        Text('mailenolan@gmail.com')
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      children: const [
+                        SizedBox(
+                          width: 100,
+                          child: Text('Phone Number'),
+                        ),
+                        Text(': '),
+                        Text('081234567890')
+                      ],
+                    )
                   ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: ElevatedButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    'Logout',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Icon(Icons.logout),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
