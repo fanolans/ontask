548d0b5012a5b0d3c445282d22da4a1d9e52bb73
fanolans <mailenolan@gmail.com>
2023-03-23 17:48:28 +0700

    create DataUser model

---
+++ lib/models/data_user.dart
@@ -0,0 +1,13 @@
+class DataUser {
+  final String username;
+  final String email;
+  final String phone;
+  final String userImageUrl;
+
+  DataUser({
+    required this.username,
+    required this.email,
+    required this.phone,
+    required this.userImageUrl,
+  });
+}
