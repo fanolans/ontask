42fe7905b40e34b1868f3afa32d45c54745dd402
fanolans <mailenolan@gmail.com>
2023-03-23 17:49:15 +0700

    add stream builder and image picker

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:ontask/services/database_service.dart';
+
+import '../models/data_user.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -9,111 +15,176 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  final _imagePicker = ImagePicker();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            height: 150,
-            color: Colors.blueAccent,
-            child: Row(
-              children: [
-                const CircleAvatar(
-                  minRadius: 40,
-                  child: Icon(
-                    Icons.person,
-                    size: 30,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(
-                  width: 15,
-                ),
-                const Text(
-                  'Farrel Nolan',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.edit),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
-            ),
-            child: Text(
-              'Data User',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Card(
-              child: Container(
-                padding: const EdgeInsets.all(20),
-                child: Column(
+      child: StreamBuilder(
+        stream: DatabaseService().dataUser,
+        builder: (context, AsyncSnapshot<DataUser> dataUserSnapshot) {
+          if (dataUserSnapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (dataUserSnapshot.hasError) {
+            return Center(
+              child: Text(
+                dataUserSnapshot.error!.toString(),
+              ),
+            );
+          }
+
+          final dataUser = dataUserSnapshot.data as DataUser;
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                height: 150,
+                color: Colors.blueAccent,
+                child: Row(
                   children: [
-                    Row(
-                      children: const [
-                        SizedBox(
-                          width: 100,
-                          child: Text('Email'),
+                    GestureDetector(
+                      onTap: () async {
+                        XFile? xFile = await _imagePicker.pickImage(
+                          source: ImageSource.gallery,
+                          imageQuality: 30,
+                        );
+                        if (xFile != null) {
+                          DatabaseService().uploadUserImage(
+                            File(xFile.path),
+                          );
+                        }
+                      },
+                      child: const CircleAvatar(
+                        minRadius: 40,
+                        child: Icon(
+                          Icons.person,
+                          size: 30,
+                          color: Colors.white,
                         ),
-                        Text(': '),
-                        Text('mailenolan@gmail.com')
-                      ],
+                      ),
                     ),
                     const SizedBox(
-                      height: 15,
+                      width: 15,
                     ),
-                    Row(
-                      children: const [
-                        SizedBox(
-                          width: 100,
-                          child: Text('Phone Number'),
-                        ),
-                        Text(': '),
-                        Text('081234567890')
-                      ],
-                    )
+                    Text(
+                      dataUser.username,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () async {
+                        String tempUsername = '';
+                        bool? isEditUsername = await showDialog(
+                          context: context,
+                          builder: (builder) {
+                            return AlertDialog(
+                              title: const Text('Edit Username'),
+                              content: TextFormField(
+                                initialValue: dataUser.username,
+                                onChanged: (value) {
+                                  tempUsername = value;
+                                },
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () =>
+                                      Navigator.of(context).pop(false),
+                                  child: const Text('Batal'),
+                                ),
+                                TextButton(
+                                  onPressed: () =>
+                                      Navigator.of(context).pop(true),
+                                  child: const Text('Simpan'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                        if (isEditUsername != null && isEditUsername) {
+                          DatabaseService().updateUsername(tempUsername);
+                        }
+                      },
+                      icon: const Icon(Icons.edit),
+                    ),
                   ],
                 ),
               ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: ElevatedButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    'Logout',
-                    style: TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                child: Text(
+                  'Data User',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Card(
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            const SizedBox(
+                              width: 100,
+                              child: Text('Email'),
+                            ),
+                            const Text(': '),
+                            Text(dataUser.email)
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          children: [
+                            const SizedBox(
+                              width: 100,
+                              child: Text('Phone Number'),
+                            ),
+                            const Text(': '),
+                            Text(dataUser.phone)
+                          ],
+                        )
+                      ],
                     ),
                   ),
-                  Icon(Icons.logout),
-                ],
+                ),
               ),
-            ),
-          ),
-        ],
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ElevatedButton(
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Logout',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(Icons.logout),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
